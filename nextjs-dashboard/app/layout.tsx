92c3f3fe0be35e93e84bdb6a86639a564d9a1a0b
CristhyanG <cristhyangiovannemg@gmail.com>
2024-09-19 22:28:58 -0300

    Continuação do projeto Dashboard next e migração para um repositório para o dashboard

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     
-      <body className={`${inter.className} antialiased`}>{children} aaaaaaa</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
